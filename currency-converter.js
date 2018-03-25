c3aee18860438f20c08ca11c19c7115ef279ec5b
MAS <digitalcolony@gmail.com>
2018-03-24 20:06:54 -0700

    Added async await versions plus error handling.

--- currency-converter.js
+++ currency-converter.js
@@ -4,46 +4,71 @@
 const axios = require("axios");
 //http://api.fixer.io/latest?base=USD
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`http://api.fixer.io/latest?base=${from}`).then(response => {
-    return response.data.rates[to];
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+    const rate = response.data.rates[to];
+    if (rate) {
+      return rate;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new Error(`Unable to get the exchange rate from ${from} to ${to}.`);
+  }
+};
+
+const getCountries = async currencyCode => {
+  try {
+    const response = await axios.get(
+      `https://restcountries.eu/rest/v2/currency/${currencyCode}`
+    );
+    return response.data.map(country => country.name);
+  } catch (e) {
+    throw new Error(
+      `Unable to get countries that use the currency code: ${currencyCode}.`
+    );
+  }
+};
+
+const convertCurrencyAlt = async (from, to, amount) => {
+  const countries = await getCountries(to);
+  const rate = await getExchangeRate(from, to);
+  const exchangedAmount = amount * rate;
+
+  return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(
+    ", "
+  )}`;
+};
+
+convertCurrencyAlt("USD", "CAD", 100)
+  .then(status => {
+    console.log(status);
+  })
+  .catch(e => {
+    console.log(e.message);
   });
-};
 
-const getCountries = currencyCode => {
-  return axios
-    .get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-    .then(response => {
-      return response.data.map(country => country.name);
-    });
-};
+// const convertCurrency = (from, to, amount) => {
+//   let countries;
+//   return getCountries(to)
+//     .then(tempCountries => {
+//       countries = tempCountries;
+//       return getExchangeRate(from, to);
+//     })
+//     .then(rate => {
+//       const exchangedAmount = amount * rate;
 
-getExchangeRate("USD", "EUR").then(rate => {
-  console.log(rate);
-});
+//       return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(
+//         ", "
+//       )}`;
+//     });
+// };
 
-const convertCurrency = (from, to, amount) => {
-  let countries;
-  return getCountries(to)
-    .then(tempCountries => {
-      countries = tempCountries;
-      return getExchangeRate(from, to);
-    })
-    .then(rate => {
-      const exchangedAmount = amount * rate;
+// getCountries("CAD").then(countries => {
+//   console.log(countries);
+// });
 
-      return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(
-        ", "
-      )}`;
-    });
-};
-
-convertCurrency("USD", "CAD", 100).then(status => {
-  console.log(status);
-});
-
-getCountries("CAD").then(countries => {
-  console.log(countries);
-});
-
-//convertCurrencyAlt 
+// getExchangeRate("USD", "EUR").then(rate => {
+//   console.log(rate);
+// });
